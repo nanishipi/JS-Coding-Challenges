0cf082a8d8ffbef8a4f8633a867fe8a808c8a3c9
André Goncalo <Andre.Goncalo@solera.com>
2023-03-29 09:10:01 +0200

    guess the number game

--- Challenges/Guess my Number/script.js
+++ Challenges/Guess my Number/script.js
@@ -2,10 +2,13 @@
 
 let message = document.querySelector(".message");
 let number = document.querySelector(".number");
-const checkBtn = document.querySelector(".check");
 
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
+const checkBtn = document.querySelector(".check");
+const againBtn = document.querySelector(".again");
+
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = document.querySelector(".highscore");
 
 checkBtn.addEventListener("click", () => {
   const guess = document.querySelector(".guess").value;
@@ -14,18 +17,17 @@ checkBtn.addEventListener("click", () => {
     message.textContent = "You did not insert a guess!";
   } else if (guess == randomNumber) {
     message.textContent = "Correct Number!";
-  } else if (guess > randomNumber) {
-    if (score > 1) {
-      message.textContent = "Try a lower number";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      message.textContent = "You lost the game!";
-      document.querySelector(".score").textContent = 0;
+    document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector(".number").style.width = "30rem";
+    number.textContent = randomNumber;
+
+    if (score > highScore.textContent) {
+      highScore.textContent = score;
     }
-  } else if (guess < randomNumber) {
+  } else if (guess !== randomNumber) {
     if (score > 1) {
-      message.textContent = "Try a higher number";
+      document.querySelector(".message").textContent =
+        guess > randomNumber ? "Try a lower number" : "Try a higher number";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
@@ -33,5 +35,15 @@ checkBtn.addEventListener("click", () => {
       document.querySelector(".score").textContent = 0;
     }
   }
-  console.log(guess);
+});
+
+againBtn.addEventListener("click", () => {
+  message.textContent = "Start guessing...";
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  number.textContent = "?";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
 });
